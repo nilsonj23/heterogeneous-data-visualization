c2c6d7766d82c028bb0d98b39fd1fd283758cec5
Nilson Junio <nilsonj23@gmail.com>
2020-09-28 18:49:43 -0300

    Feito o front-end e a criação da pagina de visualização

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Vizualização de Dados Heterogêneos' });
 });
 
+/* GET visualizacao page. */
+router.get('/visualizacao', function(req, res, next) {
+  res.render('visualizacao', { title: 'Tipos de Vizualização' });
+});
+
 module.exports = router;
